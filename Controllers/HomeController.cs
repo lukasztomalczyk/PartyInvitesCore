a28f6be5beaa298247618c26d900e267ce0bf34f
lukasz.tomalczyk <lukasz.tomalczyk@outlook.com>
2018-03-31 12:53:35 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult FormRegistration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FormRegistration(GuestResponse _guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", _guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public IActionResult Index()
+        {
+           return View();
+        }
+
+    }
+}
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+            [Required(ErrorMessage = "Proszę podać imię")]
+            public string Name { get; set; }
+            [Required(ErrorMessage = "Proszę podać Email")]
+            [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Proszę podać prawidłowy adres e-mail.")]
+            public string Email { get; set; }
+            [Required(ErrorMessage = "Proszę podać numer teelfonu")]
+            public string Phone { get; set; }
+            public bool? WillAttend { get; set; }
+    }
+}
